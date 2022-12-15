367ed12f4a47ec9c0ab21e73f1f7436f6237b476
Ian <chemwenokibet11@gmail.com>
2022-12-15 16:41:40 +0300

    made final changes to all the files

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const balance = document.getElementById('balance');
-const money_plus = document.getElementById('money-plus');
-const money_minus = document.getElementById('money-minus');
-const list = document.getElementById("list");
-const form = document.getElementById("form");
-const text = document.getElementById("text");
-const amount = document.getElementById("amount");
-const storageTransactions = JSON.parse(
-    localStorage.getItem("transactions")
+const balance = document.getElementById('balance')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
+const list = document.getElementById('list')
+const form = document.getElementById('form')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
+
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem('transactions')
 );
 
-let transactions;
-if (localStorage.getItem('transactions') !== null) {
-  transactions = storageTransactions;
-} else {
-  transactions = [];
-}
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-// Add transactions
-function addTransaction(e){
+// Add transaction
+function addTransaction(e) {
     e.preventDefault();
 
-    if(text.value.trim() === '' || amount.value.trim() === ''){
-        alert('please add a text and amount');
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
     }else {
         const transaction = {
             id: generateID(),
             text: text.value,
             amount: +amount.value
         };
+
         transactions.push(transaction);
+
         addTransactionDOM(transaction);
+
         updateValues()
+
         updateLocalStorage()
 
         text.value = ''
@@ -39,63 +39,63 @@ function addTransaction(e){
 }
 
 //generate random ID
-function generateID(){
+function generateID() {
     return Math.floor(Math.random() * 100000000);
 }
 
-//adding transactions to DOM list
-function addTransactionDOM(transaction){
+//Add transactions to DOM list
+function addTransactionDOM(transaction) {
     //get sign
     const sign = transaction.amount < 0 ? '-' : '+';
 
     const item = document.createElement('li');
 
-    //add class based on value
+    // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
     
         ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
         <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
-        `;
+    `;
 
-        list.appendChild(item);
+    list.appendChild(item);
 }
 
 //update the balance, income and expense
-function updateValues(){
+function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
 
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-    
+
     const income = amounts
         .filter(item => item > 0)
         .reduce((acc, item) => (acc += item), 0)
         .toFixed(2);
-    
+
     const expense = (
-        amounts.filter(item =>item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2)
-    
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
 
-    balance.innerHTML = `$${total}`;
+    balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
-    money_minus.innerText = `$${expense}`;    
-}    
+    money_minus.innerText = `$${expense}`;
+}
 
-// remove transaction by ID
-function removeTransaction(id){
-    transactions = transactions.filter(transaction => transaction.id !== id)
+//Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
 
     updateLocalStorage();
-    Init();
+
+    init();
 }
 
 //update local storage transactions
-function updateLocalStorage(){
+function updateLocalStorage() {
     localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
-//init app
+//Init app
 function init() {
     list.innerHTML = '';
 
